e36b0eec92f4d4fdea34f437bf6b29d636ef2aba
Philip Kung <pkung67@utexas.edu>
2021-10-31 10:55:30 -0500

    pushing query endpoint

--- backend/app/controllers/search_controller.py
+++ backend/app/controllers/search_controller.py
@@ -3,14 +3,22 @@ from ..models.user import User
 search_controller = Namespace(
     'Search', 'Search endpoint', '/search'
 )
+from ..clients import QueryPhoneNumberClient, QueryAllClient
 
 @search_controller.route('/<string:phone_number>')
 @search_controller.doc(security=None)
 class QueryPhoneNumberController(Resource):
 
-    @search_controller.response(200, 'Successfully called endpoint', hello_world_response)
+    @search_controller.response(200, 'Successfully called endpoint')
     def get(self, phone_number):
-        user = User.objects(phone_number=phone_number)
-        if user:
-            return user[0].to_json()
-        return {}
+        query_client = QueryPhoneNumberClient(phone_number)
+        return query_client.create_output()
+
+@search_controller.route('/all')
+@search_controller.doc(security=None)
+class QueryPhoneNumberController(Resource):
+
+    @search_controller.response(200, 'Successfully called endpoint')
+    def get(self):
+        query_client = QueryAllClient(User.objects)
+        return query_client.create_output()
