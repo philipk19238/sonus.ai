4d56c02c0ad3303be9e101613d405ae567040556
Philip Kung <pkung67@utexas.edu>
2021-10-30 22:06:44 -0500

    adding more changes for audio transcriber

---
+++ backend/app/global_config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    MONGODB_SETTINGS = {
+        'host': os.getenv('MONGO_HOST')
+    }
+    
+
+
+
+    
